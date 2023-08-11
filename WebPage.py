3403919d1fd0d1eaa2bee834f5b454304bf27824
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-08-12 01:38:49 +0530

    displaying movies posters using TMBD API

--- WebPage.py
+++ WebPage.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
+
+
+def fetch_poster(movie_id):
+    response = requests.get(
+        "https://api.themoviedb.org/3/movie/{}?api_key=e006e57ee414fd74972cec97c4cd8f62".format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/original" + data['poster_path']
 
 
 def recommend(movie_name):
@@ -9,11 +17,16 @@ def recommend(movie_name):
 
     top_n_movies = similarity_matrix.get(movie_index, [])
 
-    output = []
+    recommended_movie = []
+    recommended_movie_poster = []
     for index, similarity_score in top_n_movies:
-        output.append(movies.iloc[index]['title'])
+        # fetching poster from TMDB API
+        movie_id = movies.iloc[index].movie_id
+        recommended_movie_poster.append(fetch_poster(movie_id))
+
+        recommended_movie.append(movies.iloc[index]['title'])
         # finding title for the indices that were the most similar
-    return output
+    return recommended_movie, recommended_movie_poster
 
 
 # importing movie dictionary consisting of movie name & movie id
@@ -32,6 +45,12 @@ selected_movie_name = st.selectbox("Movies", movies['title'].values)
 
 # adding a button , which when pressed - will call recommend function
 if st.button("Recommend"):
-    recommendations = recommend(selected_movie_name)
-    for movie in recommendations:
-        st.write(movie)
+    names, posters = recommend(selected_movie_name)
+
+    num_columns = 5
+    col_layout = st.columns(num_columns)
+
+    for i in range(num_columns):
+        with col_layout[i]:
+            st.text(names[i])
+            st.image(posters[i])
