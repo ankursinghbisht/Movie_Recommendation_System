50e91d7ec50696d1e4190e9f1d6bc7fe29186e66
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-08-12 00:26:41 +0530

    added selectbox and recommend button

--- WebPage.py
+++ WebPage.py
@@ -2,11 +2,36 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+
+def recommend(movie_name):
+    movie_index = movies[movies['title'] == movie_name].index[0]
+    # finding the index of movie mentioned, & finding its similarity matrix
+
+    top_n_movies = similarity_matrix.get(movie_index, [])
+
+    output = []
+    for index, similarity_score in top_n_movies:
+        output.append(movies.iloc[index]['title'])
+        # finding title for the indices that were the most similar
+    return output
+
+
+# importing movie dictionary consisting of movie name & movie id
 movies_dictionary = pickle.load(open('Dataset/movies_dictionary.pkl', 'rb'))
+# converting it into pandas DF, to show it in selectbox of streamlit
 movies = pd.DataFrame(movies_dictionary)
 
+# importing similarity matrix
 similarity_matrix = pickle.load(open('Dataset/similarity_matrix.pkl', 'rb'))
 
+# declaring title of website
 st.title("Movie Recommendation System")
 
-selected_movie_name = st.selectbox("Movie", movies['title'].values)
+# showing the selectbox, with all movie options available
+selected_movie_name = st.selectbox("Movies", movies['title'].values)
+
+# adding a button , which when pressed - will call recommend function
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie_name)
+    for movie in recommendations:
+        st.write(movie)
